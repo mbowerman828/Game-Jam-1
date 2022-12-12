86d772be0f41de4f3313f568f7f2f04a5e13617c
Matt Bowerman <bowermad12@gmail.com>
2022-12-11 20:07:40 -0500

    Start Menu

---
+++ Start Menu.gd
@@ -0,0 +1,12 @@
+
+extends Control
+
+
+
+
+func _on_Button_pressed():
+	get_tree().change_scene("res://Scenes/Test.tscn")
+
+
+func _on_Button2_pressed():
+	get_tree().change_scene("res://Controls.tscn")
